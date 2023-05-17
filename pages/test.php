19f857ffef865bb6317f81459ff9e0dbc57f0462
Kwibels <yarigami092@gmail.com>
2023-05-17 21:01:13 +0700

    login html and css

---
+++ pages/test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<script>
+    function sub(){
+        alert("yes");
+    }
+</script>
+<body>
+    <div class="bg">
+    <form align="center" class="form">
+        <h1 style="color: #936D49">Login</h1><br>
+        <input class="login" type="text" placeholder="Kode Kelas"><br><br>
+        <input class="login" type="text" placeholder="Password"><br><br>
+        <input class="submit" type="button" value="Submit" onclick="sub()">
+    </form>
+    </div>
+</body>
+</html>
