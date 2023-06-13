3ca78bfe2f3a86a9c1b5148a6e3f5b1d94bb7d4f
Ng Cong Cen <57190526@student.kwikkiangie.ac.id>
2023-06-13 22:47:03 +0700

    fix endclass update record data

--- utils/endClass.php
+++ utils/endClass.php
@@ -1,17 +1,19 @@
 <?php
+include '../utils/connect.php';
+
 session_start();
 session_destroy();
-
+// attend data
 $attendList = $_POST["jsonData"];
-
 $existingArray = json_decode($attendList, true);
 foreach ($existingArray as $index => &$object) {
     $statusArray = $_POST["status"][$index];
     $object['status'] = $statusArray;
 }
-
 $modifiedJsonString = json_encode($existingArray);
-echo $modifiedJsonString;
+
+$q = "UPDATE record SET record_attend='$modifiedJsonString' WHERE record_id=2";
+mysqli_query($conn, $q);
 
 header("Location: http://localhost/sistem-presensi-rpl/pages/infoPage.php?search=1");
 exit();
